635fb7bd9e878a67c46be0477c74b8dea3c203dc
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-07-14 20:51:02 +0900

    fix skeleton code

--- programs/Hanoi2-skeleton/Hanoi.cs
+++ programs/Hanoi2-skeleton/Hanoi.cs
@@ -2,15 +2,27 @@ using System;
 
 public class Hanoi
 {
+    // The three tower objects
     private HanoiTower[] towers;
+
+    // Maximum number of disks in this Hanoi program
     public int NumOfDisks { get; private set; }
+
+    // The current number of turns in the hanoi program
     public int Turns { get; private set; }
-    
+
+    // Constructor
     public Hanoi() {...}
-    
+
+    // Method to start the hanoi program
     public void Run() {...}
+
+    // Execute a turn (recursive method) and draw the towers to the screen
     private void ExecuteTurn(int n, HanoiTower source, HanoiTower via, HanoiTower dest) {...}
+
+    // Draw the hanoi tower and disks
     public void Draw() {...}
-    
+
+    // Draw one disk (used in Draw() method)
     private void DrawDisk(int n, int diskSize) {...}
 }
--- programs/Hanoi2-skeleton/HanoiTower.cs
+++ programs/Hanoi2-skeleton/HanoiTower.cs
@@ -2,20 +2,35 @@ using System;
 
 public class HanoiTower
 {
+    // The maximum number of disks allowed in this tower
+    // readonly means a constant value (meaning that you cannot change it after initialization)
     private readonly int maxDisks;
+
+    // All the disks in this tower (stored in an array)
     public int[] Disks { get; private set; }
+
+    // The current number of disks
     public int TowerCount
     {
         get {...}
     }
 
+    // Check if the tower is empty or not
     public bool Empty
     {
         get {...}
     }
 
+    // constructor
     public HanoiTower(int maxDisks) {...}
+
+    // Insert all disks into this tower
+    // for instance, if maxDisk = 5, Disks = {5, 4, 3, 2, 1}
     public void InsertAllDisks() {...}
-    public void InsertDisk() {...}
-    public void RemoveDisk() {...}
+
+    // Insert a disk (with size diskSize) into this tower
+    public void InsertDisk(int diskSize) {...}
+
+    // Remove a disk from this tower (and return the disk's size)
+    public int RemoveDisk() {...}
 }

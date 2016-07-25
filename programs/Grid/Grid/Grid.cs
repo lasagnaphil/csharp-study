d3da2ba562461f14073990508be4ebcc8047c5ee
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-07-25 19:28:55 +0900

    working on lecture 6

---
+++ programs/Grid/Grid/Grid.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Grid
+{
+    public class Grid<T>
+    {
+        private T[,] data;
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Grid(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            data = new T[width, height];
+        }
+
+        public Grid(int width, int height, T value) : this(width, height)
+        {
+            SetAll(value);
+        }
+
+        public Grid(int width, int height, List<T> items) : this(width, height)
+        {
+            int copySize = Math.Min(width*height, items.Count);
+            for (int i = 0; i < copySize; i++)
+            {
+                int x = i%width;
+                int y = i/width;
+                data[x, y] = items[i];
+            }
+        }
+
+        public T Get(int x, int y)
+        {
+            return data[x, y];
+        }
+
+        public void Set(int x, int y, T value)
+        {
+            data[x, y] = value;
+        }
+
+        public List<T> GetItemsInRect(int x1, int y1, int x2, int y2)
+        {
+            List<T> list = new List<T>();
+            for (int x = x1; x <= x2; x++)
+            {
+                for (int y = y1; y <= y2; y++)
+                {
+                    list.Add(data[x, y]);
+                }
+            }
+            return list;
+        }
+
+        public void SetItemsInRect(int x1, int y1, int x2, int y2, T value)
+        {
+            for (int x = x1; x <= x2; x++)
+            {
+                for (int y = y1; y <= y2; y++)
+                {
+                    data[x, y] = value;
+                }
+            }
+        }
+
+        public List<T> ToList()
+        {
+            return GetItemsInRect(0, 0, Width - 1, Height - 1);
+        }
+
+        public void SetAll(T value)
+        {
+            SetItemsInRect(0, 0, Width - 1, Height - 1, value);
+        }
+    }
+}
---
+++ programs/Grid/Grid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grid<int> grid = new Grid<int>(5, 5, 1);
+            grid.Set(3, 3, 2);
+            grid.Set(3, 2, 4);
+            grid.SetItemsInRect(0, 0, 2, 2, 5);
+            int i = 1;
+            List<int> nums = grid.ToList();
+            foreach (int num in nums)
+            {
+                Console.Write(num);
+                if (i % grid.Width == 0) Console.WriteLine();
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}

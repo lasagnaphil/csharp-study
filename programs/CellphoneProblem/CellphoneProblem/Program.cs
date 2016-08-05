62428a56dd560ca245c6311933918001fcf6db83
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-08-05 16:35:53 +0900

    error fiX

--- programs/CellphoneProblem/CellphoneProblem/Program.cs
+++ programs/CellphoneProblem/CellphoneProblem/Program.cs
@@ -8,11 +8,43 @@ namespace CellphoneProblem
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Multiply(int a, int b)
         {
-
+            return a*b;
         }
 
-        static void 
+        static int Calculate(Func<int, int, int> func)
+        {
+            return func(1, 2);
+        }
+
+        static Func<int, int, int> GetFunction(char symbol)
+        {
+            switch (symbol)
+            {
+                case '+': return (a, b) => a + b;
+                case '-': return (a, b) => a - b;
+                case '*': return (a, b) => a * b;
+                case '/': return (a, b) => a / b;
+                default:
+                    return (a, b) =>
+                    {
+                        throw new Exception("Cannot parse operator " + symbol.ToString());
+                    };
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate((a, b) => a + b));
+            Console.WriteLine(Calculate(Multiply));
+
+            var addFunction = GetFunction('+');
+            var multiplyFunction = GetFunction('*');
+            Console.WriteLine(addFunction(3, 5));
+            Console.WriteLine(GetFunction('*')(3, 5));
+
+            Console.ReadKey();
+        }
     }
 }

a4d95d374bf872d7346e1e04d5b42975515ebba8
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-08-06 23:45:40 +0900

    starting work on lecture 8

--- programs/CellphoneProblem/CellphoneProblem/Program.cs
+++ programs/CellphoneProblem/CellphoneProblem/Program.cs
@@ -6,6 +6,42 @@ using System.Threading.Tasks;
 
 namespace CellphoneProblem
 {
+    struct Vector2i
+    {
+        public int x;
+        public int y;
+
+        public Vector2i(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public static Vector2i operator +(Vector2i a, Vector2i b)
+        {
+            return new Vector2i(a.x + b.x, a.y + b.y);
+        }
+
+        public static Vector2i operator -(Vector2i a, Vector2i b)
+        {
+            return new Vector2i(a.x - b.x, a.y - b.y);
+        }
+
+        public static Vector2i operator *(Vector2i a, int k)
+        {
+            return new Vector2i(k*a.x, k*a.y);
+        }
+
+        public static Vector2i operator *(int k, Vector2i a)
+        {
+            return new Vector2i(k*a.x, k*a.y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", x, y);
+        }
+    }
+
     class Program
     {
         static int Multiply(int a, int b)
@@ -35,6 +71,19 @@ namespace CellphoneProblem
         }
 
         static void Main(string[] args)
+        {
+            Vector2i a = new Vector2i(1, 2);
+            Vector2i b = new Vector2i(3, 4);
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * 2);
+            Console.WriteLine(3 * b);
+
+            Console.ReadKey();
+        }
+
+        static void Test()
         {
             Console.WriteLine(Calculate((a, b) => a + b));
             Console.WriteLine(Calculate(Multiply));
